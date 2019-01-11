740238e72372b421509878065703bfe58140002c
unknown <kilq2reaper@hotmail.com>
2019-01-11 16:50:39 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+app.use((req,res,next) => {
+    let now = new Date().toString();
+
+     let log = now +':' + req.method + req.url;
+    fs.appendFile('server.log', log +'\n', (err) => {
+        if(err){
+            console.log('unable to append server.log');
+        }
+    });
+
+    next();
+});
+// app.use((req,res,next) => {
+//   res.render('maintenence.hbs', {
+//       maintenence:'PLease come back later'
+//   });
+
+// });
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', ()=> {
+    return new Date().getFullYear();
+
+});
+app.get('/', (req,res)=>{
+    res.render('index.hbs', {
+        pageTitle:'Welcome Page',
+
+        welcomeMessage:'Welcome to the train wreck!'
+    });
+});
+app.get('/about', (req,res) =>{
+    res.render('about.hbs', { 
+        pageTitle:'About Page',
+
+    });
+});
+app.get('/bad', (req,res) =>{
+    res.send('ERROR 101 BITCH');
+});
+app.listen(3000, ()=>{
+    console.log('server up on port 3000');
+});
